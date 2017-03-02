31a47c703f854f0431bb63a9bec7c7bf7da3aed5
VillipJohn <villipivan@gmail.com>
2017-03-02 16:54:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pc_4/funnyanecdotes11/data/DataBaseHelper.java
@@ -0,0 +1,145 @@
+package com.example.pc_4.funnyanecdotes11.data;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by PC-4 on 28.02.2017.
+ */
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    // путь к базе данных вашего приложения
+    private static String DB_PATH;
+    private static String DB_NAME = "anecdotesdb";
+    private SQLiteDatabase myDataBase;
+    private final Context mContext;
+
+    /**
+     * Конструктор
+     * Принимает и сохраняет ссылку на переданный контекст для доступа к ресурсам приложения
+     * @param context
+     */
+    public DataBaseHelper(Context context) {
+        super(context, DB_NAME, null, 12);
+        this.mContext = context;
+        DB_PATH = "/data/data/" + context.getPackageName() + "/databases/" + DB_NAME;
+        Log.v("LogDataBaseHelper", DB_PATH);
+    }
+
+    /**
+     * Создает пустую базу данных и перезаписывает ее нашей собственной базой
+     * */
+    public void createDataBase() throws IOException {
+        boolean dbExist = checkDataBase();
+
+
+
+        if(dbExist){
+            Log.v("myLogDBH", "база существует");
+            //ничего не делать - база уже есть
+        }else {
+            //вызывая этот метод создаем пустую базу, позже она будет перезаписана
+            this.getReadableDatabase();
+
+            try {
+                copyDataBase();
+            } catch (IOException e) {
+                throw new Error("Error copying database");
+            }
+        }
+
+    }
+
+    /**
+     * Проверяет, существует ли уже эта база, чтобы не копировать каждый раз при запуске приложения
+     * @return true если существует, false если не существует
+     */
+    private boolean checkDataBase(){
+        SQLiteDatabase checkDB = null;
+
+        try{
+            String myPath = DB_PATH;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+        }catch(SQLiteException e){
+            //база еще не существует
+        }
+        if(checkDB != null){
+            checkDB.close();
+        }
+        return checkDB != null;
+    }
+
+    /**
+     * Копирует базу из папки assets заместо созданной локальной БД
+     * Выполняется путем копирования потока байтов.
+     * */
+    private void copyDataBase() throws IOException{
+        //Открываем локальную БД как входящий поток
+        InputStream myInput = mContext.getAssets().open(DB_NAME);
+
+        //Путь ко вновь созданной БД
+        String outFileName = DB_PATH;
+
+        //Открываем пустую базу данных как исходящий поток
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        //перемещаем байты из входящего файла в исходящий
+        byte[] buffer = new byte[10];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+
+        //закрываем потоки
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
+    public void openDataBase() throws SQLException {
+        //открываем БД
+        String myPath = DB_PATH;
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+    }
+
+    @Override
+    public synchronized void close() {
+        if(myDataBase != null)
+            myDataBase.close();
+        super.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (newVersion > oldVersion)
+            try {
+                copyDataBase();
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+    }
+
+    // Здесь можно добавить вспомогательные методы для доступа и получения данных из БД
+    // вы можете возвращать курсоры через "return myDataBase.query(....)", это облегчит их использование
+    // в создании адаптеров для ваших view
+
+    public Cursor query(String table, String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy) {
+        return myDataBase.query("maintable", null, null, null, null, null, null);
+    }
+}
---
+++ app/src/main/java/com/example/pc_4/funnyanecdotes11/view/ItemListActivity.java
@@ -0,0 +1,172 @@
+package com.example.pc_4.funnyanecdotes11.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.pc_4.funnyanecdotes11.R;
+import com.example.pc_4.funnyanecdotes11.data.DataBaseHelper;
+import com.example.pc_4.funnyanecdotes11.data.AnecdoteContent;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * An activity representing a list of Items. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link ItemDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class ItemListActivity extends AppCompatActivity {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    Cursor c = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                *//*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*//*
+
+                    DataBaseHelper myDbHelper = new DataBaseHelper(ItemListActivity.this);
+                    try {
+                        myDbHelper.createDataBase();
+                    } catch (IOException ioe) {
+                        throw new Error("Unable to create database");
+                    }
+                    try {
+                        myDbHelper.openDataBase();
+                    } catch (SQLException sqle) {
+                        throw sqle;
+                    }
+                    Toast.makeText(ItemListActivity.this, "Successfully Imported", Toast.LENGTH_SHORT).show();
+
+                    Log.v("myLog", "Successgully Imported");
+
+                    c = myDbHelper.query("maintable", null, null, null, null, null, null);
+                    if (c.moveToFirst()) {
+                        do {
+                            Toast.makeText(ItemListActivity.this,
+                                    "_id: " + c.getString(0) + "\n" +
+                                            "CATEGORY: " + c.getString(1) + "\n" +
+                                            "ID CATEGORY: " + c.getString(2) + "\n" +
+                                            "FAVORITE:  " + c.getString(3) + "\n" +
+                                            "TEXT:  " + c.getString(4),
+                                    Toast.LENGTH_LONG).show();
+                        } while (c.moveToNext());
+                    }
+            }
+        });*/
+
+        View recyclerView = findViewById(R.id.item_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+
+        if (findViewById(R.id.item_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(AnecdoteContent.ITEMS));
+    }
+
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<AnecdoteContent.DummyItem> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<AnecdoteContent.DummyItem> items) {
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mIdView.setText(mValues.get(position).id);
+            holder.mContentView.setText(mValues.get(position).content);
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mTwoPane) {
+                        Bundle arguments = new Bundle();
+                        arguments.putString(ItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+                        ItemDetailFragment fragment = new ItemDetailFragment();
+                        fragment.setArguments(arguments);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.item_detail_container, fragment)
+                                .commit();
+                    } else {
+                        Context context = v.getContext();
+                        Intent intent = new Intent(context, ItemDetailActivity.class);
+                        intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+
+                        context.startActivity(intent);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mIdView;
+            public final TextView mContentView;
+            public AnecdoteContent.DummyItem mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mIdView = (TextView) view.findViewById(R.id.id);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
+    }
+}
