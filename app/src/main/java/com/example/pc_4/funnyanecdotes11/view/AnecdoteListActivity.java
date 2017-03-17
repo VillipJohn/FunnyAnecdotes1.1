ba75a80c3f263f6d1b2c2d2f9376ec787192e593
VillipJohn <villipivan@gmail.com>
2017-03-17 17:15:35 +0200

    Added Volley and develop retrieving data from wp api

--- app/src/main/java/com/example/pc_4/funnyanecdotes11/view/AnecdoteListActivity.java
+++ app/src/main/java/com/example/pc_4/funnyanecdotes11/view/AnecdoteListActivity.java
@@ -6,17 +6,26 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.pc_4.funnyanecdotes11.R;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
  * An activity representing a list of Items. This activity
@@ -34,6 +43,19 @@ public class AnecdoteListActivity extends AppCompatActivity {
      */
     private boolean mTwoPane;
 
+    int page = 1;
+    //String url = "http://www.anekdotabc.com.ua/wp-json/wp/v2/posts?per_page=100&page="+page+"&fields=id,content,categories";
+    List<Object> list;
+    Gson gson;
+    //ProgressDialog progressDialog;
+    //ListView postList;
+    Map<String,Object> mapPost;
+    Map<String,String> mapContent;
+    ArrayList<Object> mapCategory;
+    int postID;
+    int postCategory;
+    String postContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +72,14 @@ public class AnecdoteListActivity extends AppCompatActivity {
             // activity should be in two-pane mode.
             mTwoPane = true;
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //new downloadAsyncTask().execute();
+        onRequest();
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
@@ -142,4 +172,133 @@ public class AnecdoteListActivity extends AppCompatActivity {
             }
         }
     }
+
+    private void onRequest(){
+        onRequestLoop();
+    }
+
+    private void onRequestLoop() {
+        String url = "http://www.anekdotabc.com.ua/wp-json/wp/v2/posts?per_page=100&page="+page+"&fields=id,content,categories";
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                //if(s.equals("[]")) return null;
+                gson = new Gson();
+                list = (List) gson.fromJson(s, List.class);
+                //postTitle = new String[list.size()];
+
+                for(int i=0;i<list.size();++i){
+                    mapPost = (Map<String,Object>)list.get(i);
+                    mapContent = (Map<String, String>) mapPost.get("content");
+                    postContent = mapContent.get("rendered");
+                    postID = ((Double)mapPost.get("id")).intValue();
+
+                    mapCategory = (ArrayList<Object>) mapPost.get("categories");
+
+                    if(mapCategory != null){
+                        postCategory = ((Double)mapCategory.get(0)).intValue();
+                        //Log.v("TAG_MainActivity", "postCategory= " + postCategory);
+                    }
+
+                    //Log.v("TAG_MainActivity", "postContent= " + postContent);
+                    Log.v("TAG_MainActivity", "postID= " + postID);
+                }
+
+                Log.v("TAG_MainActivity", "page= " + page);
+
+               /* page++;
+                onRequest();*/
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.v("TAG_MainActivity", "Сработал блок ошибки");
+            }
+        });
+
+        RequestQueue rQueue = Volley.newRequestQueue(getApplicationContext());
+        rQueue.add(request);
+
+    }
+
+   /* private class downloadAsyncTask extends AsyncTask<Void, Void, Void> {
+        int page = 1;
+        String url = "http://www.anekdotabc.com.ua/wp-json/wp/v2/posts?per_page=100&page="+page+"&fields=id,content,categories";
+        List<Object> list;
+        Gson gson;
+        //ProgressDialog progressDialog;
+        //ListView postList;
+        Map<String,Object> mapPost;
+        Map<String,String> mapContent;
+        ArrayList<Object> mapCategory;
+        int postID;
+        int postCategory;
+        String postContent;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            // [... Выполните задачу в фоновом режиме, обновите переменную myProgress...]
+            // [... Продолжение выполнения фоновой задачи ...]
+            // Верните значение, ранее переданное в метод onPostExecute
+
+                StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        //if(s.equals("[]")) return null;
+                        gson = new Gson();
+                        list = (List) gson.fromJson(s, List.class);
+                        //postTitle = new String[list.size()];
+
+                        for(int i=0;i<list.size();++i){
+                            mapPost = (Map<String,Object>)list.get(i);
+                            mapContent = (Map<String, String>) mapPost.get("content");
+                            postContent = mapContent.get("rendered");
+                            postID = ((Double)mapPost.get("id")).intValue();
+
+                            mapCategory = (ArrayList<Object>) mapPost.get("categories");
+
+                            if(mapCategory != null){
+                                postCategory = ((Double)mapCategory.get(0)).intValue();
+                                Log.v("TAG_MainActivity", "postCategory= " + postCategory);
+                            }
+
+                            Log.v("TAG_MainActivity", "postContent= " + postContent);
+                            Log.v("TAG_MainActivity", "postID= " + postID);
+                        }
+
+                        page++;
+
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Log.v("TAG_MainActivity", "Сработал блок ошибки");
+                    }
+                });
+
+                RequestQueue rQueue = Volley.newRequestQueue(getApplicationContext());
+                rQueue.add(request);
+
+
+
+
+
+           *//* postList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    mapPost = (Map<String,Object>)list.get(position);
+                    postID = ((Double)mapPost.get("id")).intValue();
+
+                    Intent intent = new Intent(getApplicationContext(),Post.class);
+                    intent.putExtra("id", ""+postID);
+                    startActivity(intent);
+                }
+            });*//*
+            return null;
+        }
+    }*/
 }
